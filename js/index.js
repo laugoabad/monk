06deeb0f1848e8527c141084b589d850d6768425
laugoabad <36644178+laugoabad@users.noreply.github.com>
2021-02-10 23:22:39 -0300

    g

--- js/index.js
+++ js/index.js
@@ -34,38 +34,33 @@ function slideBackwards() {
 //******function that slides by clicking on the slide indicator  */
 function changeFrameSlide(elem) {
     slideNumber = elem.id;
-
-    console.log('prev ', prevSlideNumber, 'number ', slideNumber);
     animateSlide(slideNumber * pixelsToMove);
     textAppears(slideNumber, prevSlideNumber);
     markFrame(slideNumber, prevSlideNumber);
     createSlideNumberFooter(slideNumber);
     prevSlideNumber = slideNumber;
 }
-
+ //Chooses the elements to be shwon as the slides passes
 function textAppears(textNumber, prevTextNumber) {
-    console.log('textapp ', prevTextNumber, textNumber);
     document.getElementById('text-' + prevTextNumber).style.display = 'none';
     document.getElementById('text-' + textNumber).style.display = 'block';
     
     if (textNumber == 0) {
-        console.log('entra 0');
         document.getElementById('foot-slide-text').style.display = 'none';
         document.getElementById('0').classList.add('square');
         document.getElementById('next').style.display = "block";
         document.getElementById('prev').style.display = 'none';
         document.getElementById('text-0').style.display = 'block';
         document.getElementById('story').style.display = "block";
-
+        document.getElementById('foot').style.display = "block";
 
     } else if (textNumber == 9) {
-        console.log('entra 9');
-        document.getElementById('foot-slide-text').style.display = 'block';
+        document.getElementById('foot-slide-text').style.display = 'none';
         document.getElementById('next').style.display = 'none';
         document.getElementById('prev').style.display = 'block';
         document.getElementById('story').style.display = "none";
+        document.getElementById('loading-monk').style.display = "none";
     } else if (textNumber > 0 && textNumber < 9) {
-        console.log('entra <>');
         document.getElementById('foot-slide-text').style.display = 'block';
         document.getElementById('story').style.display = "none";
         document.getElementById('next').style.display = "block";
@@ -73,19 +68,18 @@ function textAppears(textNumber, prevTextNumber) {
     }
 }
 
-
 //******function that changes the white marks in the frame list */
 function markFrame(id, prev) {
     document.getElementById(id).classList.add('square');
     document.getElementById(prev).classList.remove('square');
 }
-
+//Creates the index to be shon in the text aside the list of frames
 function createSlideNumberFooter(slideNumber) {
     if (slideNumber >= 1 && slideNumber <= 8) {
         document.getElementById('path').innerHTML = slideNumber;
     }
 }
-
+ // animates the slider
 function animateSlide(margin) {
     document.getElementById("back").animate([
         // keyframes  
@@ -99,19 +93,20 @@ function animateSlide(margin) {
 }
 
 function start() {
-    //turns invisible the backwards arrow at the begining of the slide
     window.onload = function () { document.getElementById("loading-monk").style.display = "none" }
     document.getElementById('0').classList.add('square');
     textAppears(slideNumber, prevSlideNumber);
-
-    if (screen.width > 1024) {
-        pixelsToMove = 860
+    //decides the lenght to be slide in each frame dependending on the screen width
+    if (screen.width >=1440) {
+        pixelsToMove = 880
+    }else if (screen.width > 1024 && screen.width < 1440)  {
+        pixelsToMove = 830
     } else {
         if (screen.width <= 1024 && screen.width > 425) {
-            pixelsToMove = 810;
+            pixelsToMove = 820;
         } else {
             if (screen.width <= 425) {
-                pixelsToMove = 820;
+                pixelsToMove = 840;
                 document.getElementById('text').style.margintop = '0px'
             }
         }
